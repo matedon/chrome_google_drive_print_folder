8b83fe0a58ed5f9f230b2345a5e0118a2b76b94b
matedon <matedon@gmail.com>
2021-02-04 07:55:48 +0100

    Add english language to dics file. Remove unnecessary attribute from that.

--- js/dics_files.js
+++ js/dics_files.js
@@ -1,231 +1,189 @@
 window.dics_files = [
   {
-    "language": "Arabic",
+    "symbol": "en",
+    "text": "Folder"
+  },
+  {
     "symbol": "ar",
     "text": "الملفات"
   },
   {
-    "language": "Dutch",
     "symbol": "nl",
     "text": "bestanden"
   },
   {
-    "language": "French",
     "symbol": "fr",
     "text": "des dossiers"
   },
   {
-    "language": "German",
     "symbol": "de",
     "text": "Dateien"
   },
   {
-    "language": "Italian",
     "symbol": "it",
     "text": "File"
   },
   {
-    "language": "Japanese",
     "symbol": "ja",
     "text": "ファイル"
   },
   {
-    "language": "Russian",
     "symbol": "ru",
     "text": "файлы"
   },
   {
-    "language": "Spanish",
     "symbol": "es",
     "text": "archivos"
   },
   {
-    "language": "Bulgarian",
     "symbol": "bg",
     "text": "файлове"
   },
   {
-    "language": "Chinese Traditional",
     "symbol": "zh-tw",
     "text": "檔案"
   },
   {
-    "language": "Danish",
     "symbol": "da",
     "text": "filer"
   },
   {
-    "language": "Estonian",
     "symbol": "et",
     "text": "faile"
   },
   {
-    "language": "Hebrew",
     "symbol": "iw",
     "text": "קבצים"
   },
   {
-    "language": "Lithuanian",
     "symbol": "lt",
     "text": "failus"
   },
   {
-    "language": "Marathi",
     "symbol": "mr",
     "text": "फायली"
   },
   {
-    "language": "Portuguese",
     "symbol": "pt",
     "text": "arquivos"
   },
   {
-    "language": "Serbian",
     "symbol": "sr",
     "text": "фајлови"
   },
   {
-    "language": "Tamil",
     "symbol": "ta",
     "text": "கோப்புகள்"
   },
   {
-    "language": "Turkish",
     "symbol": "tr",
     "text": "Dosyalar"
   },
   {
-    "language": "Vietnamese",
     "symbol": "vi",
     "text": "các tập tin"
   },
   {
-    "language": "Ukrainian",
     "symbol": "uk",
     "text": "файлів"
   },
   {
-    "language": "Telugu",
     "symbol": "te",
     "text": "ఫైళ్లు"
   },
   {
-    "language": "Swedish",
     "symbol": "sv",
     "text": "filer"
   },
   {
-    "language": "Slovak",
     "symbol": "sk",
     "text": "súbory"
   },
   {
-    "language": "Romanian",
     "symbol": "ro",
     "text": "fișiere"
   },
   {
-    "language": "Norwegian",
     "symbol": "no",
     "text": "filer"
   },
   {
-    "language": "Malay",
     "symbol": "ms",
     "text": "fail"
   },
   {
-    "language": "Korean",
     "symbol": "ko",
     "text": "파일"
   },
   {
-    "language": "Indonesian",
     "symbol": "id",
     "text": "file"
   },
   {
-    "language": "Filipino",
     "symbol": "tl",
     "text": "mga file"
   },
   {
-    "language": "Croatian",
     "symbol": "hr",
     "text": "datoteke"
   },
   {
-    "language": "Catalan",
     "symbol": "ca",
     "text": "Fitxers"
   },
   {
-    "language": "Bengali",
     "symbol": "bn",
     "text": "নথি পত্র"
   },
   {
-    "language": "Chinese Simplified",
     "symbol": "zh-cn",
     "text": "档案"
   },
   {
-    "language": "Czech",
     "symbol": "cs",
     "text": "soubory"
   },
   {
-    "language": "Esperanto",
     "symbol": "eo",
     "text": "dosieroj"
   },
   {
-    "language": "Finnish",
     "symbol": "fi",
     "text": "tiedostot"
   },
   {
-    "language": "Greek",
     "symbol": "el",
     "text": "αρχεία"
   },
   {
-    "language": "Latvian",
     "symbol": "lv",
     "text": "failus"
   },
   {
-    "language": "Malayalam",
     "symbol": "ml",
     "text": "ഫയലുകൾ"
   },
   {
-    "language": "Polish",
     "symbol": "pl",
     "text": "akta"
   },
   {
-    "language": "Slovenian",
     "symbol": "sl",
     "text": "datotek"
   },
   {
-    "language": "Tajik",
     "symbol": "tg",
     "text": "файлҳо"
   },
   {
-    "language": "Thai",
     "symbol": "th",
     "text": "ไฟล์"
   },
   {
-    "language": "Urdu",
     "symbol": "ur",
     "text": "فائلوں"
   },
   {
-    "language": "Hungarian",
     "symbol": "hu",
     "text": "fájlok"
   }
